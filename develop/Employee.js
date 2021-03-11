20a5614206e15923a15a8d0331817ceb100d1ff5
Sharon <martinezsharonr@gmail.com>
2021-03-10 21:15:46 -0800

    added pseudo code and starter files

---
+++ develop/Employee.js
@@ -0,0 +1,13 @@
+// create classes for each so those tests pass
+
+//then index.js write code and use inquirer to  get answers 
+
+// based on those answers call constructor to create new inter, manager, engineer 
+
+//pass in those properties to get your manager, enginner, intern objects
+
+// then call constructors and all their values
+
+// employee is a parent class 
+
+// all other are subclasses
