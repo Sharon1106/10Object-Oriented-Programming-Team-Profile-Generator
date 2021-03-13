48adb1108084ae7a7be1257a2cc7414293458589
Sharon <martinezsharonr@gmail.com>
2021-03-12 20:25:30 -0800

    added employee class and more pseudocode

---
+++ src/page-template.js
@@ -0,0 +1,10 @@
+// create the team based on the answered questions from  prompt
+
+// generate team using arrow function 
+const generateTeam = team => {
+  // that returns template literal 
+  return 
+  // enter generated html inside ``
+  ``
+
+}
